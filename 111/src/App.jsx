5c5ddf3501f8b31e5797b67123e36b57167bb816
Voldemarrrs <vovattn123@gmail.com>
2024-03-04 17:18:56 +0300

    dfklfdnjodfsn

---
+++ 111/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/clad.jpg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+   
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
